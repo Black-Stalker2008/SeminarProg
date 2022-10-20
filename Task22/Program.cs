a2b48568eecabb79edceeead23262ffb5ba5cefc
BlackStalker2008 <114511758+Black-Stalker2008@users.noreply.github.com>
2022-10-21 00:12:24 +0500

    Решение 22 задачи

---
+++ Task22/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+void Table(int n)
+{
+    if (n > 0)
+    {
+        int index = 1;
+        while (index <= n)
+        {
+            Console.WriteLine($"{index,4} {index*index,4}");
+            index++;
+        }
+
+    } 
+    else Console.WriteLine("Некорректное число");
+}
+
+
+Console.WriteLine("Введите натуральное число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Table(n);
+
